a20643182d5a20c1c8f1ba0f758cdb9189bd26a4
WalidAlrefai <w.alrefai.3@gmail.com>
2022-05-08 14:55:38 +0300

    make like history

--- src/components/Results/results.js
+++ src/components/Results/results.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./results.scss"
 import JSONPretty from 'react-json-pretty';
-
 function Result(props) {
     if (!props.loading) {
         return (
             <div className="result">
                 <div className="api">
-                    {
-                    <p className="method" style={props.method === "POST" ? { backgroundColor: 'green' } : props.method === "DELETE" ? { backgroundColor: 'red' } : props.method === "PUT" ? { backgroundColor: 'yellow' } : {}}>{props.method}</p>
-                    }
-                    <p>{props.url}</p>
+                    {/* {console.log(props.methods, 'methods11')} */}
+                    <div className='map'>
+                        {
+                            props.methods.map(e => {
+                                return (
+                                    <div className="MM">
+                                            <p className="method" style={e.method === "POST" ? { backgroundColor: 'green' } : e.method === "DELETE" ? { backgroundColor: 'red' } : e.method === "PUT" ? { backgroundColor: 'yellow' } : {}}>{e.method}</p>
+                                            <p >{e.url}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
                 <div className="body">
                     <JSONPretty data={props.header} />
